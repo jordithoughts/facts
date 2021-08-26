70403ce0907a96037d2d3d1912efc00f29f5b5f0
jordithoughts <jordi.thoughts@gmail.com>
2021-08-26 17:06:57 +0200

    2 files modified, 1 added

---
+++ src/components/Command.vue
@@ -0,0 +1,18 @@
+template>
+  <div>
+    <h1>Enter your command: {{msg}}</h1>
+</template>
+
+<script>
+export default {
+  name: 'Command',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+/*
+<style scoped>
+
+</style>*/
